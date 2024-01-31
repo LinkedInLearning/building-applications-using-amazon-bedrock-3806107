ae6b1f929618be9c64c760e2a4cded13bdfb5300
Lee Assam <leeassam@gmail.com>
2024-01-31 00:44:26 -0500

    Removed knowledge base id

--- 02_04e/langchain-knowledge-base-sample-interactive.py
+++ 02_04e/langchain-knowledge-base-sample-interactive.py
@@ -5,7 +5,7 @@ from langchain_community.retrievers import AmazonKnowledgeBasesRetriever
 
 #Define the retriever
 retriever = AmazonKnowledgeBasesRetriever(
-    knowledge_base_id="P4P5UVX2UB",
+    knowledge_base_id="",
     retrieval_config={"vectorSearchConfiguration": {"numberOfResults": 4}},
 )
 
