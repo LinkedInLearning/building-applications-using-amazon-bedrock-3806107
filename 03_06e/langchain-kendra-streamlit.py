5608565c7d16e1bac481a8c46dfd004822b0877f
Lee Assam <leeassam@gmail.com>
2024-02-01 21:35:53 +0000

    added final kendra file

--- 03_06e/langchain-kendra-streamlit.py
+++ 03_06e/langchain-kendra-streamlit.py
@@ -9,8 +9,8 @@ from langchain.prompts.prompt import PromptTemplate
 from langchain.chains.conversational_retrieval.prompts import CONDENSE_QUESTION_PROMPT
 
 #Configure streamlit app
-st.set_page_config(page_title="Employee HR Bot", page_icon="📖")
-st.title("📖 Employee HR Bot")
+st.set_page_config(page_title="Employee HR Bot")
+st.title("Employee HR Bot")
 
 #Define the retriever
 retriever = AmazonKendraRetriever(
@@ -19,16 +19,20 @@ retriever = AmazonKendraRetriever(
 )
 
 #Define model parameters
-model_kwargs = {"temperature": 0, "top_k": 10, "max_tokens_to_sample": 3000}
+model_kwargs_claude = {
+  "temperature" : 0,
+  "top_k" : 10,
+  "max_tokens_to_sample" : 750
+}
 
 #Configure llm
-llm = Bedrock(model_id="anthropic.claude-instant-v1", model_kwargs=model_kwargs)
+llm = Bedrock(model_id="anthropic.claude-instant-v1", model_kwargs=model_kwargs_claude)
 
 #Set up message history
-msgs = StreamlitChatMessageHistory(key="langchain_messages")
-memory = ConversationBufferMemory(chat_memory=msgs, memory_key = 'chat_history', output_key='answer', return_messages=True)
+msgs = StreamlitChatMessageHistory(key = "langchain_messages")
+memory = ConversationBufferMemory(chat_memory=msgs, memory_key='chat_history', output_key='answer', return_messages=True)
 if len(msgs.messages) == 0:
-    msgs.add_ai_message("How can I help you?")
+  msgs.add_ai_message("How can I help you?")
 
 #Creating the template   
 my_template = """
@@ -46,30 +50,31 @@ Assistant:
 """
 
 #configure prompt template
-prompt_template = PromptTemplate( 
-            input_variables=[ 'context', 'chat_history', 'question'],    
-            template=my_template)
+prompt_template = PromptTemplate(
+  input_variables= ['context', 'chat_history', 'question'],
+  template= my_template
+)
 
 #Configure the chain
 qa = ConversationalRetrievalChain.from_llm(
-    llm=llm, 
-    retriever=retriever, 
-    return_source_documents=True,
-    combine_docs_chain_kwargs={"prompt": prompt_template},
-    memory = memory,
-    condense_question_prompt=CONDENSE_QUESTION_PROMPT
+  llm = llm,
+  retriever= retriever,
+  return_source_documents = True,
+  combine_docs_chain_kwargs= {"prompt" : prompt_template},
+  memory = memory,
+  condense_question_prompt= CONDENSE_QUESTION_PROMPT
 )
 
 #Render current messages from StreamlitChatMessageHistory
 for msg in msgs.messages:
-    st.chat_message(msg.type).write(msg.content)
+  st.chat_message(msg.type).write(msg.content)
 
 #If user inputs a new prompt, generate and draw a new response
 if prompt := st.chat_input():
-    st.chat_message("human").write(prompt)
+  st.chat_message("human").write(prompt)
 
-    #Messages are saved to history automatically by Langchain during run
-    output = qa.invoke({'question':prompt, 'chat_history':memory.load_memory_variables({})})
+  #Invoke the model
+  output = qa.invoke({'question' : prompt, 'chat_history' : memory.load_memory_variables({})})
     
-    #display the output
-    st.chat_message("ai").write(output['answer'])
+  #display the output
+  st.chat_message("ai").write(output['answer'])  
