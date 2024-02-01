9f9026985d32a1304bf9b36596e03f1cede6a3d9
Lee Assam <leeassam@gmail.com>
2024-01-31 18:06:57 -0800

    Added starter code for langchain-knowledge-base-streamlit.py

--- 02_06b/langchain-knowledge-base-streamlit.py
+++ 02_06b/langchain-knowledge-base-streamlit.py
@@ -1,16 +1,38 @@
 #Imports
+from langchain.chains import RetrievalQA
+from langchain_community.llms import Bedrock
+from langchain_community.retrievers import AmazonKnowledgeBasesRetriever
+import streamlit as st
+from langchain.memory import ConversationBufferMemory
+from langchain_community.chat_message_histories import StreamlitChatMessageHistory
+from langchain.prompts.prompt import PromptTemplate
 
 #Configure streamlit app
 
+
 #Define the retriever
+retriever = AmazonKnowledgeBasesRetriever(
+    knowledge_base_id="",
+    retrieval_config={"vectorSearchConfiguration": {"numberOfResults": 4}},
+)
 
 #Define model parameters
+model_kwargs_claude = {
+  "temperature" : 0,
+  "top_k" : 10,
+  "max_tokens_to_sample" : 750
+}
 
 #Configure llm
+llm = Bedrock(model_id="anthropic.claude-instant-v1", model_kwargs=model_kwargs_claude)
 
 #Set up message history
 
 #Creating the template   
+my_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Keep the answer as concise as possible. 
+{context}
+Question: {question}
+Helpful Answer:"""  
 
 #Configure prompt template
 
